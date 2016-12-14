0d6fe7f6e76d47b8b48546c7da982f47b3ad5d78
ssbadala <ssbadala@gmail.com>
2016-12-14 13:55:03 -0800

    This is the first change

---
+++ GitSampleProject/src/mainFile.java
@@ -0,0 +1,13 @@
+public class mainFile {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+	}
+
+	public static void displayGreeting(){
+		System.out.println("I am in main class");
+		System.out.println("Test 1");
+	}
+}
